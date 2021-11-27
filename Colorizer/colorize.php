bd37596d9cd1cc4d1a9aec6ede91e93f6466e0d2
Jahanvi Patel <jahanvi316@gmail.com>
2021-11-26 23:35:54 -0500

    Delete Colorizer directory

--- Colorizer/colorize.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo("colorize.php called");
-// call file
-    $output = shell_exec("python colorizer.py");
-    header("Location: results.html");
-    exit;
-?>
--- Colorizer/decolorize.php
+++
@@ -1,7 +0,0 @@
-<?php
-    echo("decolorize.php called");
-// call file
-    $output = shell_exec("python decolorizer.py");
-    header("Location: results.html");
-    exit;
-?>
